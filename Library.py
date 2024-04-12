9f006cabc290ccfb831ceed34aa958ab41828fed
KousalyaKarthik <166791520+KousalyaKarthik@users.noreply.github.com>
2024-04-12 11:43:54 +0530

    Add files via upload

---
+++ Library.py
@@ -0,0 +1,13 @@
+class SubfieldsInAI:
+    def SubFields():
+        subFieldList=["Machine Learning","Neural Networks","Vision","Robotics","Speech Processing","Natural Language Processing"]
+        print("Sub-fields in AI are:")
+        for x in subFieldList:        
+            print(x)
+            
+    def OddEven():
+        i=int(input("Enter a number:"))
+        if(i%2==1):
+            print(i,"is Odd Number")
+        else:
+            print(i,"is Even Number")
